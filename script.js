490c83ce500639020f3d8e522f3570736b8d3a34
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-07-15 13:17:32 -0500

    added JS to fetch images from Unsplash API

--- script.js
+++ script.js
@@ -25,8 +25,52 @@ const fixedOnScroll = () =>{
     }
 }
 
+const getImages = async () => {
+    const API_KEY = '6W37YsXGmsBvGwCjnwwUW2lMcylWHmBWM0H1MqPDuUs';
+
+    const basURL = 'https://api.unsplash.com/search/photos/';
+    const query = `?client_id=${API_KEY}&query=furniture&orientation=landscape&ar=4:3&fit=crop?per_page=10`;
+
+    const response = await fetch(basURL + query);
+
+    if (response.status !== 200) {
+        throw new Error('there was an error getting the information');
+    }
+
+   const data = await response.json();
+
+   console.log(data);
+
+   return data;
+}
+
+
+const updateUI =  (data) => {
+
+    const {total, total_pages, results} = data;
+
+    const hero = document.querySelector('section.hero');
+    const gridImg = document.querySelectorAll('.img-features div.grid-img img');
+    const moreImg = document.querySelectorAll('.more-img img');
+
+    hero.style.backgroundImage = `url(${results[0].urls.regular})`;
+
+    for(let index = 0; index < gridImg.length; index++){
+        gridImg[index].src = results[index + 1].urls.regular;
+    }
+
+    for (let index = 0; index < moreImg.length; index++) {
+        moreImg[index].src = results[gridImg.length + index].urls.regular;
+    }
+}
+
 openMenu();
+
+getImages()
+    .then(data => updateUI(data))
+    .catch(err => console.log(err.message));
 
 onscroll = () => {
     fixedOnScroll();
 }
+
